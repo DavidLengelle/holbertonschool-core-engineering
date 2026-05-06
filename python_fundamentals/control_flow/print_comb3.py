754e5bcf9c90b865dbaf77fe74f003fd1049760a
David Lengellé <david.lengelle.1.10@gmail.com>
2026-05-06 14:45:33 +0200

    fix print_comb3.py to correctly format output for combinations of two digits

--- python_fundamentals/control_flow/print_comb3.py
+++ python_fundamentals/control_flow/print_comb3.py
@@ -2,5 +2,7 @@
 
 for i in range(9):
     for j in range(i + 1, 10):
-        print("{0}{1}, ".format(i, j), end="")
-print("{0}{1}".format(i, j))
+        if i == 8 and j == 9:
+            print("{0}{1}".format(i, j))
+        else:
+            print("{0}{1}, ".format(i, j), end="")
