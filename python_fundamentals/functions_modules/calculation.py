e077715f8c54a0c02fab0fba3582cb59baece355
David Lengellé <david.lengelle.1.10@gmail.com>
2026-05-07 12:55:29 +0200

    refactor calculation.py to move variable initialization and print statements inside the main block

--- python_fundamentals/functions_modules/calculation.py
+++ python_fundamentals/functions_modules/calculation.py
@@ -2,10 +2,12 @@
 
 import calculator_1 as calc
 
-a = 10
-b = 5
+if __name__ == "__main__":
 
-print("{} + {} = {}".format(a, b, calc.add(a, b)))
-print("{} - {} = {}".format(a, b, calc.sub(a, b)))
-print("{} * {} = {}".format(a, b, calc.mul(a, b)))
-print("{} / {} = {}".format(a, b, calc.div(a, b)))
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".format(a, b, calc.add(a, b)))
+    print("{} - {} = {}".format(a, b, calc.sub(a, b)))
+    print("{} * {} = {}".format(a, b, calc.mul(a, b)))
+    print("{} / {} = {}".format(a, b, calc.div(a, b)))
