b4d694acdde4a442a7588253e91235aed8e3ef16
David Lengellé <david.lengelle.1.10@gmail.com>
2026-05-09 17:24:49 +0200

    add: implement common_elements function to return common elements between two sets

---
+++ python_fundamentals/core_data_structures/common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Return common elements between two sets."""
+
+
+def common_elements(set_1, set_2):
+    """Return a new set with elements present in both set_1 and set_2."""
+
+    return set_1 & set_2
