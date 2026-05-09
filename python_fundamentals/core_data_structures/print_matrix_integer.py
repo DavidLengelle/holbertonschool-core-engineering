2b42cea1ba4a8b528b5619502b3d912d9f3675a3
David Lengellé <david.lengelle.1.10@gmail.com>
2026-05-09 16:51:47 +0200

    add print_matrix_integer.py to print a matrix of integers

---
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""print a matrix of integers"""
+
+
+def print_matrix_integer(matrix=[[]]):
+    """Print each row of matrix; values separated by a single space."""
+
+    for row in matrix:
+        for i in range(len(row)):
+            if i == len(row) - 1:
+                print("{:d}".format(row[i]))
+            else:
+                print("{:d}".format(row[i]), end=" ")
