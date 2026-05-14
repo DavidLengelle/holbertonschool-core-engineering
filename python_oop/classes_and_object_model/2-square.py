59a7b21f187c3ec6ad370ce2d71a4273c4077ea9
David Lengellé <david.lengelle.1.10@gmail.com>
2026-05-14 15:49:58 +0200

    add: implement Square class with validated size attribute

---
+++ python_oop/classes_and_object_model/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module that defines a Square class with a validated size attribute"""
+
+
+class Square:
+    """Square class with a validated private size attribute"""
+
+    def __init__(self, size=0):
+        """Initialize the square after validating size"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
