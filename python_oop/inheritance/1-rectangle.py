f4f37e77f297428af140ddf457e98e1c030a2f8a
David Lengellé <david.lengelle.1.10@gmail.com>
2026-05-15 12:02:53 +0200

    add: implement Rectangle class inheriting from BaseGeometry with validation

---
+++ python_oop/inheritance/1-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Definition de la classe Rectangle qui herite de BaseGeometry."""
+BaseGeometry = __import__('base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle defini par une largeur et une hauteur, valeurs validees."""
+
+    def __init__(self, width, height):
+        """Initialise un Rectangle avec width et height entiers positifs."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
